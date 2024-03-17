077e45acf70d697b7a67c88c6f70dca40eb2e4bf
TonySMK <tonysun25light@live.ca>
2024-03-17 18:12:18 -0400

    update...

---
+++ service_worker.js
@@ -0,0 +1,8 @@
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: () => {
+//       alert("hello from the extension!");
+//     },
+//   });
+// });
